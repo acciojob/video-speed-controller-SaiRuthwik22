ea233e16ba1fb74058067eda7fb614114f962e44
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-07-22 19:20:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const inputs = document.querySelectorAll('.controls input');
+function pauseVideo(){
+	let video = document.getElementById("video")
+	if(!video.paused){
+		video.pause()
+		console.log(video.currentTime)
+	}else{
+		video.play()
+	}
+}
+function forward(){
+	let video = document.getElementById("video")
+	video.currentTime+=25
+}
+function backward(){
+	let video = document.getElementById("video")
+	video.currentTime -=10
+}
+function updateVolume() {
+    let video = document.getElementById("video");
+    let volumeControl = document.getElementById("volumeControl");
+    video.volume = volumeControl.value;
+}
 
+function updatePlaybackRate() {
+    let video = document.getElementById("video");
+    let playbackRateControl = document.getElementById("playbackRateControl");
+    video.playbackRate = playbackRateControl.value;
+}
     function handleUpdate() {
       const suffix = this.dataset.sizing || '';
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
